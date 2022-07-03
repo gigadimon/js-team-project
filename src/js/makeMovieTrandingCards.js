b083a2b90fd04c3d66d2d8154ec6280918c0d31b
gigadimon <gigadimon@gmail.com>
2022-07-03 19:01:59 +0300

    add refactored movie tranding cards code

--- src/js/makeMovieTrandingCards.js
+++ src/js/makeMovieTrandingCards.js
@@ -1,11 +1,3 @@
-import * as basicLightbox from 'basiclightbox';
-
-const instance = basicLightbox.create(`
-    <img src="assets/images/image.png" width="800" height="600">
-`);
-
-instance.show();
-
 export default function makeMovieTrandingCards(
   {
     id,
