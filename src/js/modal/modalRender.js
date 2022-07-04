9c4662330bd76784b73acd36a79c721c6b929bb8
Yurii Denkovych <yd2004@ukr.net>
2022-07-04 22:50:53 +0300

    Update modalRender.js

--- src/js/modal/modalRender.js
+++ src/js/modal/modalRender.js
@@ -5,7 +5,10 @@ let qwe = document.querySelector('.container');
 qwe.addEventListener('click', target)
 function target(e){
    e.preventDefault();
-  if(e.target.nodeName === 'IMG')
+   if(e.target.nodeName !== 'IMG'){
+    return;
+   }
+  
    console.log(e.target.getAttribute('data-id'))
    backdrop.classList.remove('is-hidden');
 }
