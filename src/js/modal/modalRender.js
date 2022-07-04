8c1c61f0025766266120703947c66e3fc28125d6
Yurii Denkovych <yd2004@ukr.net>
2022-07-04 22:36:56 +0300

    Create modalRender.js

---
+++ src/js/modal/modalRender.js
@@ -0,0 +1,11 @@
+import {backdrop} from './modal-close';
+
+let qwe = document.querySelector('.container');
+
+qwe.addEventListener('click', target)
+function target(e){
+   e.preventDefault();
+  if(e.target.nodeName === 'IMG')
+   console.log(e.target.getAttribute('data-id'))
+   backdrop.classList.remove('is-hidden');
+}
