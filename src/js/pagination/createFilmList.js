997f440e78bdcb37afceb44294bc7e852d5d5d8c
Vitaliy Yerin <37743681+VitaliyYerin@users.noreply.github.com>
2022-07-07 09:12:19 +0300

    Add pagiantions styles

--- src/js/pagination/createFilmList.js
+++ src/js/pagination/createFilmList.js
@@ -4,7 +4,7 @@ import Pagination from './Pagination';
 
 const cardSection = document.querySelector('.body-container');
 <<<<<<< HEAD
-const logo = document.querySelector(".header__logo")
+const logo = document.querySelector(".header__logo");
 const INITIAL_PAGE_NUMBER = 1;
 =======
 const logo = document.querySelector('.header__logo');
