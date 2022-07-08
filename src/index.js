1604f396018b19081e16bd90d30efcfc49ee20b3
Vladyslav Komar <valdek125@gmail.com>
2022-07-08 18:57:37 +0300

    Update  Notiflix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import renderModal from './js/modal/modalRender';
+import notiflixOptions from './js/notiflix/notiflixOptions'
 import loadingPage from './js/current-session/loadingPage';
 import currentSession from "./js/current-session/currentSession"
 import header from './js/header/header';
