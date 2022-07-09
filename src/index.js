389dee8cc8339a61cbe38340e1b6cc4992fbba7e
Kilivnik <ekstasy1990@gmail.com>
2022-07-10 00:41:59 +0300

    add footer-modal

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import renderModal from './js/modal/modalRender';
-import notiflixOptions from './js/notiflix/notiflixOptions'
+import notiflixOptions from './js/notiflix/notiflixOptions';
 import loadingPage from './js/current-session/loadingPage';
-import currentSession from "./js/current-session/currentSession"
+import currentSession from './js/current-session/currentSession';
 import header from './js/header/header';
 import header from './js/header/darkMode';
+import footer from './js/footer-modal/footerModal';
 
-loadingPage()
-
+loadingPage();
