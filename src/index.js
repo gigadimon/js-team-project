eb5785d94d0ad6c5c55db56c61bcb6b14b41b62f
Vitaliy Yerin <37743681+VitaliyYerin@users.noreply.github.com>
2022-07-09 17:44:21 +0300

    change index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import notiflixOptions from './js/notiflix/notiflixOptions'
 import loadingPage from './js/current-session/loadingPage';
 import currentSession from "./js/current-session/currentSession"
 import header from './js/header/header';
-import header from './js/header/darkMode';
-import scrooll from './js/scrooll-arrow/scrollArrow';
+import darkMode from './js/header/darkMode';
+import scrollArrow from './js/scrooll-arrow/scrollArrow';
 
 loadingPage()
 
