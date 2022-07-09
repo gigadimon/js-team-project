98a5c500bcfd3e031789c38e61349de033b8a125
gigadimon <gigadimon@gmail.com>
2022-07-09 22:11:07 +0300

    fix scroll

--- src/js/scrooll-arrow/scrollArrow.js
+++ src/js/scrooll-arrow/scrollArrow.js
@@ -1,7 +1,7 @@
-const scrollBtn = document.querySelector(".back-to-top");
+const scrollBtn = document.querySelector('.back-to-top');
 
 const btnVisibility = () => {
-  if (window.scrollY > 50) {
+  if (window.scrollY > 0) {
     scrollBtn.classList.add('show');
   } else {
     scrollBtn.classList.remove('show');
@@ -15,10 +15,10 @@ function scrollUp() {
   });
 }
 
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   btnVisibility();
 });
-scrollBtn.addEventListener("click", (e) => {
+scrollBtn.addEventListener('click', e => {
   e.preventDefault();
   scrollUp();
 });
