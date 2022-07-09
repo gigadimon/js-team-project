60902605dd78e219dbc4a369ca6aaaee55be9c88
Vitaliy Yerin <37743681+VitaliyYerin@users.noreply.github.com>
2022-07-09 19:44:18 +0300

    Update scrollArrow

--- src/js/scrooll-arrow/scrollArrow.js
+++ src/js/scrooll-arrow/scrollArrow.js
@@ -1,24 +1,24 @@
 const scrollBtn = document.querySelector(".back-to-top");
 
-  const btnVisibility = () => {
+const btnVisibility = () => {
   if (window.scrollY > 50) {
-  scrollBtn.classList.add('show');
-} else {
-  scrollBtn.classList.remove('show');
-}
+    scrollBtn.classList.add('show');
+  } else {
+    scrollBtn.classList.remove('show');
+  }
 };
 
-  function scrollUp() {
+function scrollUp() {
   window.scrollTo({
     top: 0,
     behavior: 'smooth',
   });
 }
 
-  document.addEventListener("scroll", () => {
+document.addEventListener("scroll", () => {
   btnVisibility();
 });
-  document.addEventListener("click", (e) => {
+scrollBtn.addEventListener("click", (e) => {
   e.preventDefault();
   scrollUp();
 });
