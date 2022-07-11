98a57124914ae0a91934dab766f8442e8802905e
Kilivnik <ekstasy1990@gmail.com>
2022-07-12 01:18:37 +0300

    add ua/en lang

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import footer from './js/footer-modal/footerModal';
 import makeMyLibraryCards from './js/my-library/makeMyLibraryCards';
 import darkMode from './js/header/darkMode';
 import scrollArrow from './js/scrooll-arrow/scrollArrow';
-import inputSaver from './js/film-search/inputSaver'
+import inputSaver from './js/film-search/inputSaver';
+import language from './js/languages/language';
 
 loadingPage();
